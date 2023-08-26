c059e0d86f300ea6a48733e7c755c54ceae785e1
Jan1598 <jan.heinrich98@web.de>
2023-08-26 15:48:51 +0200

    Removed annotation from Application
    - This annotation provides the unexpected creation of the dev-bundle

--- src/main/java/com/bbs/filmdistribution/Application.java
+++ src/main/java/com/bbs/filmdistribution/Application.java
@@ -4,7 +4,9 @@ import com.bbs.filmdistribution.data.service.SamplePersonRepository;
 import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.theme.Theme;
+
 import javax.sql.DataSource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.sql.init.SqlDataSourceScriptDatabaseInitializer;
@@ -13,28 +15,31 @@ import org.springframework.context.annotation.Bean;
 
 /**
  * The entry point of the Spring Boot application.
- *
+ * <p>
  * Use the @PWA annotation make the application installable on phones, tablets
  * and some desktop browsers.
- *
  */
 @SpringBootApplication
-@NpmPackage(value = "@fontsource/roboto", version = "4.5.0")
-@Theme(value = "filmdistribution")
-public class Application implements AppShellConfigurator {
+@Theme( value = "filmdistribution" )
+public class Application implements AppShellConfigurator
+{
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+    public static void main( String[] args )
+    {
+        SpringApplication.run( Application.class, args );
     }
 
     @Bean
-    SqlDataSourceScriptDatabaseInitializer dataSourceScriptDatabaseInitializer(DataSource dataSource,
-            SqlInitializationProperties properties, SamplePersonRepository repository) {
+    SqlDataSourceScriptDatabaseInitializer dataSourceScriptDatabaseInitializer( DataSource dataSource, SqlInitializationProperties properties, SamplePersonRepository repository )
+    {
         // This bean ensures the database is only initialized when empty
-        return new SqlDataSourceScriptDatabaseInitializer(dataSource, properties) {
+        return new SqlDataSourceScriptDatabaseInitializer( dataSource, properties )
+        {
             @Override
-            public boolean initializeDatabase() {
-                if (repository.count() == 0L) {
+            public boolean initializeDatabase()
+            {
+                if ( repository.count() == 0L )
+                {
                     return super.initializeDatabase();
                 }
                 return false;
